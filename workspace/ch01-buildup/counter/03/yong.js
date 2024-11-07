3a09cac00768f2cb7b5c14d621b845af1ed37c2f
yeon1ee <carpediem.y13@gmail.com>
2024-11-07 15:50:01 +0900

    ✨ 1107 수업 내용 - 컴포넌트로 사용하기

---
+++ workspace/ch01-buildup/counter/03/yong.js
@@ -0,0 +1,25 @@
+const yong = {
+  //지정한 속성과 자식 요소를 가지는 요소 노드를 생성해서 반환 함
+  createElement: (tag, props, ...children) => {
+    //요소 노드 생성
+    const elem = document.createElement(tag);
+
+    //속성 추가
+    if(props) {
+      for (const attrName in props) {
+        elem.setAttribute(attrName, props[attrName]);
+      }
+    }
+
+    //자식 노드 추가
+    for (let child of children) {
+      if (typeof child === 'string' || typeof child === 'number') {
+        child = document.createTextNode(child);
+      }
+      elem.appendChild(child);
+    }
+    return elem;
+  }
+}
+
+export default yong;
