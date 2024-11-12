0d1d0a057feb54237a395c9e0c50df89a55b1b0f
yeon1ee <carpediem.y13@gmail.com>
2024-11-12 16:07:59 +0900

    ✨ 1112 수업 내용

---
+++ workspace/ch02-start/props/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+import Counter from "./components/Counter";
+
+function App() {
+  return (  
+    <div id="app">
+      <Header />
+      <Counter />
+    </div>);
+}
+
+export default App
