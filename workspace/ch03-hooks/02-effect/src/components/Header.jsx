82d44c4baad453f1bd9c3058d7f8c06283d94710
yeon1ee <carpediem.y13@gmail.com>
2024-11-20 15:56:09 +0900

    ✨ 1120 수업 내용

---
+++ workspace/ch03-hooks/02-effect/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return(
+  <header>
+    <h1>03 useReducer - 상태 관리 로직은 한 곳에서</h1>
+  </header>
+  );
+}
+
+export default Header;
---
+++ workspace/ch03-hooks/04-useRef/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return(
+  <header>
+    <h1>04 useRef - 데이터 관리</h1>
+  </header>
+  );
+}
+
+export default Header;
