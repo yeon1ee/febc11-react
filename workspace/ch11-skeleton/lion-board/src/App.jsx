9d5952b43ea91cbe6fde0171091162edf26f358c
yeon1ee <carpediem.y13@gmail.com>
2024-12-12 10:44:42 +0900

    🚧

--- workspace/ch11-skeleton/lion-board/src/App.jsx
+++ workspace/ch11-skeleton/lion-board/src/App.jsx
@@ -1,6 +1,6 @@
 import { RouterProvider } from "react-router-dom";
 import router from '@/routes.jsx';
-import useThemeStore from "@zustand/themeStore";
+import useThemeStore from "@zustand/themeStore.js";
 
 function App() {
 
--- workspace/ch11-skeleton/lion-board/vite.config.js
+++ workspace/ch11-skeleton/lion-board/vite.config.js
@@ -1,33 +1,17 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'  // https://vite.dev/config/
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [
-      { find: "@", replacement: "src" },
-      { find: "@components", replacement: "src/components" },
-      { find: "@pages", replacement: "src/pages" },
-      { find: "@hooks", replacement: "src/hooks" },
-      { find: "@zustand", replacement: "src/zustand" },
-      { find: "@recoil", replacement: "src/recoil" },
+      { find: "@", replacement: "/src" },
+      { find: "@components", replacement: "/src/components" },
+      { find: "@pages", replacement: "/src/pages" },
+      { find: "@hooks", replacement: "/src/hooks" },
+      { find: "@zustand", replacement: "/src/zustand" },
+      { find: "@recoil", replacement: "/src/recoil" },
     ],
   },
 })
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   resolve: {
-//     alias: [
-//       { find: "@", replacement: "/src" },
-//       { find: "@components", replacement: "/src/components" },
-//       { find: "@pages", replacement: "/src/pages" },
-//       { find: "@hooks", replacement: "/src/hooks" },
-//       { find: "@zustand", replacement: "/src/zustand" },
-//       { find: "@recoil", replacement: "/src/recoil" },
-//     ],
-//   },
-// })
