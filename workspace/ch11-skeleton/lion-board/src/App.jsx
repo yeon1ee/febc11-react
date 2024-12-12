44550586fb7f8ec69c2eae1dad07983258e3f2f7
yeon1ee <carpediem.y13@gmail.com>
2024-12-12 10:42:06 +0900

    🚧

--- workspace/ch11-skeleton/lion-board/src/App.jsx
+++ workspace/ch11-skeleton/lion-board/src/App.jsx
@@ -1,5 +1,5 @@
 import { RouterProvider } from "react-router-dom";
-import router from '@/routes';
+import router from '@/routes.jsx';
 import useThemeStore from "@zustand/themeStore";
 
 function App() {
